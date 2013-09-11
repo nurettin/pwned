1b0ba303371a8e442a774f5e49572a408d24482a
Nurettin Onur TUĞCU <onurtugcu@gmail.com>
2013-09-11 07:45:05 +0300

    added curl::open

---
+++ curl/tests.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "curl.hpp"
+
+int main()
+{
+	/* TODO: add test to check for exception and returned data
+	std::string result= pwned::curl::open("http://google.com");
+	std::cout<< result<< '\n';
+	*/
+}
+

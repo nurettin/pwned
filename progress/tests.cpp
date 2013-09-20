c9d5dbf5e564301d5792744692c38fe328174196
Nurettin Onur TUĞCU <onurtugcu@maltepe.edu.tr>
2013-09-20 14:29:44 +0300

    progress setw [ci skip]

--- progress/tests.cpp
+++ progress/tests.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <iomanip>
 #include <chrono>
+#include <ctime>
 
 struct Progress
 {
@@ -35,7 +37,7 @@ struct Progress
     (this->* tick_ptr)();
   }
 
-  std::chrono::steady_clock::time_point
+  std::chrono::milliseconds
   estimate() const
   {
     int64_t elapsed_milliseconds= std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()- start).count();
@@ -43,13 +45,26 @@ struct Progress
     if(elapsed_milliseconds> 0)
       average_tick_milliseconds= ticks/ elapsed_milliseconds;
     uint64_t remaining_ticks= total_ticks- ticks;
-    return start+ std::chrono::duration_cast<std::chrono::steady_clock::duration>(std::chrono::milliseconds(average_tick_milliseconds* remaining_ticks));
+    return std::chrono::milliseconds(average_tick_milliseconds* remaining_ticks);
   }
 
   void print() const
   {
-    estimate();
-    std::cout<< "(elapsed) |==============>                | (percent) (estimated)\r";
+    auto estimate_ms= estimate();
+    auto ms= estimate_ms % 1000;
+    estimate_ms-= ms;
+    auto s= std::chrono::duration_cast<std::chrono::seconds>(estimate_ms);
+    estimate_ms-= s;
+    auto m= std::chrono::duration_cast<std::chrono::minutes>(estimate_ms);
+    estimate_ms-= m;
+    auto h= std::chrono::duration_cast<std::chrono::hours>(estimate_ms);
+    std::cout<< "(elapsed) |==============>                | (percent) ("
+      << std::setfill('0')
+      << std::setw(2)
+      << h.count()<< ":"<< m.count()<< ":"<< s.count()<< "."
+      << std::setw(3)
+      << ms.count() 
+      << ")\r";
   }
 };
 

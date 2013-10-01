161032139dcdb4ccb23ebf4fbd068858e97e6139
Nurettin Onur TUĞCU <onurtugcu@maltepe.edu.tr>
2013-10-01 13:35:10 +0300

    crawl wikipedia example added

---
+++ examples/formicator/crawl_wikipedia.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <pwned/curl/formicator.hpp>
+
+int main()
+{
+  // finds turkish names for countries by crawling wikipedia
+  pwned::curl::Formicator crawl("http://tr.wikipedia.org");
+  crawl.get("wiki/%C3%9Clkeler_listesi");
+  auto links= crawl.select("table.wikitable td:nth-child(2) > a");
+  
+  for(auto &a: links)
+  {
+    if(a== 0) continue;
+    auto &data= a-> data;
+    data.parseAttributes();
+    auto attr= data.attributes();
+    std::cout<< attr["title"]<< '\n';
+  }
+}
+

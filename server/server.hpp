831881f9d3c9222d808b1be40fce7d0e8f6dfd9d
Nurettin Onur <onurtugcu@gmail.com>
2013-11-01 20:42:40 +0200

    fix server header bug

--- server/server.hpp
+++ server/server.hpp
@@ -139,8 +139,8 @@ struct Server
     std::ostringstream out;
     out<< "HTTP/1.1 "<< status<< "\r\n"
       << "Content-Type: "<< content_type<< "\r\n"
-      << "Content-Length: "<< content.size()<< "\r\n\r\n"
-      << "Server: pwned (backed by mongoose "<< mg_version()<< ')'
+      << "Content-Length: "<< content.size()<< "\r\n"
+      << "Server: pwned (backed by mongoose "<< mg_version()<< ")\r\n\r\n"
       << content;
     return out.str();
   }
